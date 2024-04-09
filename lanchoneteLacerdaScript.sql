879ed0cbe500ccab8bfad27956e66d5613b09417
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-04-09 15:30:25 -0300

    Update

---
+++ lanchoneteLacerdaScript.sql
@@ -0,0 +1,24 @@
+create database lanchoneteLacerda;
+
+use lanchoneteLacerda;
+
+create table produtos (
+id int not null auto_increment,
+nome varchar(200) not null,
+preco decimal(10,2) not null,
+ingredientes varchar(500) not null,
+imagem blob not null,
+primary key(id)
+);
+
+create table clientes (
+id int not null auto_increment,
+foto blob not null,
+nome varchar(200) not null,
+endereco varchar(120) not null,
+telefone varchar(14) not null,
+email varchar(120) not null unique,
+cpf varchar(11) not null unique,
+password varchar(100) not null,
+primary key(id)
+);
